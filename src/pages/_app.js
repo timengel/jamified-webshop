de2c02632a8ff00ca075566e68999ec0f59a3ac4
Tim Engel <tim.engel.lp@gmail.com>
2021-11-08 21:05:25 +0100

    refactor home page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Layout from '../components/layout/Layout';
 import '../styles/Colors.css';
 import './App.css';
+import Head from 'next/head';
 
 const JamifiedWebshopApp = ({Component, pageProps}) => {
   return (
       <Layout>
+        <Head>
+          <title>Jamified Webshop</title>
+          <link rel='icon' href='/favicon-t.svg'/>
+        </Head>
         <Component {...pageProps}/>
       </Layout>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,15 @@
-import Head from 'next/head';
 import Link from 'next/link';
+import {home} from './Home.module.css';
 
 function Home() {
   return (
-      <div>
-        <Head>
-          <title>Create Next App</title>
-          <link rel='icon' href='/favicon-t.svg'/>
-        </Head>
-
-        <main>
-          <h1>
-            Welcome to the Jamified Webshop!
-          </h1>
-
-          <p>
-            Please visit the <Link href='/products'>Products Page</Link>.
-          </p>
-        </main>
-
-        <footer>
-          <a
-              href='https://timengel.me/'
-              target='_blank'
-              rel='noopener noreferrer'
-          >
-            Powered by{' '}
-            <img src='/vercel.svg' alt='Tim'/>
-          </a>
-        </footer>
+      <div className={home}>
+        <h1>
+          Welcome to the Jamified Webshop!
+        </h1>
+        <p>
+          Please visit the <Link href='/products'>Products Page</Link>.
+        </p>
       </div>
   );
 }
