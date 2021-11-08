40c0e87b1fe8cb59298804996cf18459ea3ea00f
Tim Engel <tim.engel.lp@gmail.com>
2021-11-08 20:41:24 +0100

    refactor navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,20 +1,36 @@
-import classes from './Navbar.module.css';
+import {
+  navBar,
+  websiteLogo,
+  navCell,
+  navCellContainer,
+  logoText,
+} from './Navbar.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
+import logoPic from '../../../public/png/logo_jamified-webshop.png';
 
 const Navbar = () => {
 
   return (
-      <header className={classes.navBar}>
-        <div className={classes.logo}>Jamified Webshop</div>
-        <nav>
-          <ul>
-            <li>
-              <Link href='/'>Home</Link>
-            </li>
-            <li>
-              <Link href='/products'>Product List</Link>
-            </li>
-          </ul>
+      <header className={navBar}>
+        <div className={websiteLogo}><Image src={logoPic}
+                                            alt='Jamified Webshop'
+                                            height='100px'
+                                            width='100px'
+                                            placeholder='blur'/><span
+            className={logoText}>Jamified<br/>Webshop</span>
+        </div>
+        <nav className={navCellContainer}>
+          <div className={navCell}>
+            <Link href='/'>
+              <button>Home</button>
+            </Link>
+          </div>
+          <div className={navCell}>
+            <Link href='/products'>
+              <button>Products</button>
+            </Link>
+          </div>
         </nav>
       </header>
   );
