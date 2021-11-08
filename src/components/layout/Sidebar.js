e82eec60a52d6b91c7d6ad0b174eacacb30329af
Tim Engel <tim.engel.lp@gmail.com>
2021-11-09 00:54:05 +0100

    add styled sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,10 +1,46 @@
 import React from 'react';
-import {sideBar} from './Sidebar.module.css';
+import {
+  sidebar,
+  sidebarSection,
+  lastSidebarSection
+} from './Sidebar.module.css';
+import {FaMountain} from 'react-icons/fa';
+import {
+  MdSportsTennis,
+  MdSportsBasketball,
+  MdBusinessCenter
+} from 'react-icons/md';
+import {IconContext} from 'react-icons';
+
+const sectionIcons = [
+  <MdBusinessCenter/>,
+  <MdSportsBasketball/>,
+  <MdSportsTennis/>,
+  <FaMountain/>,
+];
+
+const iconSizePx = '50';
 
 const Sidebar = () => {
   return (
-      <div className={sideBar}>
-        THIS IS THE SIDEBAR
+      <div className={sidebar}>
+        {
+          sectionIcons.map((icon, index) => {
+            return (
+                <div className={`
+                ${sidebarSection}
+                ${index === (sectionIcons.length - 1)
+                    ? lastSidebarSection
+                    : ''}`}>
+                  <IconContext.Provider
+                      value={{size: iconSizePx}}>
+                    {icon}
+                  </IconContext.Provider>
+                </div>
+            )
+                ;
+          })
+        }
       </div>
   );
 };
