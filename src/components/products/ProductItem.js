87ba4bcc38443bc0d050422e5249c60f7a4addaf
Tim Engel <tim.engel.lp@gmail.com>
2021-11-09 01:20:47 +0100

    refactor product item, add static image

--- src/components/products/ProductItem.js
+++ src/components/products/ProductItem.js
@@ -1,18 +1,23 @@
 import Card from '../ui/Card';
-import {actions, content, item, itemImage} from './ProductItem.module.css';
+import {detailBtn, content, item, itemImage} from './ProductItem.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
 
 function ProductItem({id, title, address, image}) {
   return (
       <li className={item}>
         <Card>
           <div className={itemImage}>
-            <img src={image} alt={title}/></div>
+            <Image src={image}
+                   alt={title}
+                   width='500'
+                   height='300'/>
+          </div>
           <div className={content}>
             <h3>{title}</h3>
             <address>{address}</address>
           </div>
-          <div className={actions}>
+          <div className={detailBtn}>
             <Link href={`/products/${id}`}>
               <button>Show Details</button>
             </Link>
--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,53 +1,54 @@
 import React from 'react';
 import ProductList from '../../components/products/ProductList';
 
+const DUMMY_IMG_PATH = '/jpg/dummy_image_500-300.jpg'; // Random image URL: 'https://picsum.photos/500/300'
 const DUMMY_PRODUCTS = [
   {
     id: '1',
     title: 'First Item',
-    image: 'https://picsum.photos/500/300',
+    image: DUMMY_IMG_PATH,
     price: '30',
     description: 'This is the first product.'
   },
   {
     id: '2',
     title: 'Second Item',
-    image: 'https://picsum.photos/500/300',
+    image: DUMMY_IMG_PATH,
     price: '25',
     description: 'This is the second product.'
   },
   {
     id: '3',
     title: 'Third Item',
-    image: 'https://picsum.photos/500/300',
+    image: DUMMY_IMG_PATH,
     price: '50',
     description: 'This is the third product.'
   },
   {
     id: '4',
     title: 'Fourth Item',
-    image: 'https://picsum.photos/500/300',
+    image: DUMMY_IMG_PATH,
     price: '12',
     description: 'This is the fourth product.'
   },
   {
     id: '5',
     title: 'Fifth Item',
-    image: 'https://picsum.photos/500/300',
+    image: DUMMY_IMG_PATH,
     price: '69',
     description: 'This is the fifth product.'
   },
   {
     id: '6',
     title: 'Sixth Item',
-    image: 'https://picsum.photos/500/300',
+    image: DUMMY_IMG_PATH,
     price: '17',
     description: 'This is the sixth product.'
   },
   {
     id: '7',
     title: 'Seventh Item',
-    image: 'https://picsum.photos/500/300',
+    image: DUMMY_IMG_PATH,
     price: '99',
     description: 'This is the seventh product.'
   }
