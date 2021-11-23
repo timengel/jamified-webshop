2ca68d9b4bfc6ad3dc0ca4e43e2e8dba7acb88a5
Tim Engel <tim.engel.lp@gmail.com>
2021-11-23 18:14:34 +0100

    remove fetch metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,17 @@
 import Link from 'next/link';
 import {home} from '../styles/Home.module.scss';
-import {fetchApi} from '../../lib/api';
 
-const Home = ({global}) => {
+const Home = () => {
   return (
       <div className={home}>
         <h1>
-          Welcome to the {global.siteName}!
+          Welcome to the Jamified Webshop!
         </h1>
         <p>
           Please visit the <Link href='/products'>Products Page</Link>.
         </p>
       </div>
   );
-}
-
-export async function getStaticProps() {
-  const global = await fetchApi('/global');
-  // const faviconUrl = getStrapiMedia(global.favicon);
-  return {
-    props: {
-      global: global,
-    }
-  };
 }
 
 export default Home;
