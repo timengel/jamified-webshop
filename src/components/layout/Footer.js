7468e0a6f9ee8abf09db5a90eab23e84cba4310d
Tim Engel <tim.engel.lp@gmail.com>
2022-02-21 14:37:55 +0100

    make footer hydration state visible

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,8 +2,19 @@ import React from 'react';
 import {footer} from './Footer.module.scss';
 
 const Footer = () => {
+  const [didHydrate, setDidHydrate] = React.useState(false);
+  React.useEffect(() => {
+    setDidHydrate(true);
+    console.log('Hydrated: ProductItem ' + slug)
+  }, []);
+
   return (
-      <div className={footer}>
+      <div className={footer}
+           style={{
+             background: didHydrate
+                 ? "#4beaa9"
+                 : "#ff788c",
+           }}>
         <a href='https://www.timengel.me/'>
           © Tim Engel
         </a>
