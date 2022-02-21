9bc7be1ca78452a029f7c860d3b26c68aa4b2a79
Tim Engel <tim.engel.lp@gmail.com>
2022-02-21 14:49:06 +0100

    fix error

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,19 +2,8 @@ import React from 'react';
 import {footer} from './Footer.module.scss';
 
 const Footer = () => {
-  const [didHydrate, setDidHydrate] = React.useState(false);
-  React.useEffect(() => {
-    setDidHydrate(true);
-    console.log('Hydrated: ProductItem ' + slug)
-  }, []);
-
   return (
-      <div className={footer}
-           style={{
-             background: didHydrate
-                 ? "#4beaa9"
-                 : "#ff788c",
-           }}>
+      <div className={footer}>
         <a href='https://www.timengel.me/'>
           © Tim Engel
         </a>
